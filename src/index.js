35c897c4ecc93667a2772c815af0647c5a772c09
ksenia <arfloger@gmail.com>
2019-10-18 01:47:28 +0300

    task 8

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import {App} from './components/app/app.jsx';
+import {movieTitles} from './mock';
 
 const init = () => {
   ReactDOM.render(
-      <App />,
+      <App
+        movieTitles = {movieTitles}
+      />,
       document.querySelector(`#root`)
   );
 };
