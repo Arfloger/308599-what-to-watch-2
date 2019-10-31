84e465c69a23c50340af6b5184be4c1f4471b9f3
ksenia <arfloger@gmail.com>
2019-10-31 20:53:14 +0300

    task 14 add tests

--- src/components/small-movie-card/small-movie-card.e2e.test.js
+++ src/components/small-movie-card/small-movie-card.e2e.test.js
@@ -21,3 +21,30 @@ it(`SmallMovieCard is correctly`, () => {
   expect(mouseHoverHandler).toHaveBeenCalledTimes(1);
   expect(mouseHoverHandler).toHaveBeenCalledWith(film);
 });
+
+it(`onMouseEnter event is correctly`, () => {
+  const film = films[0];
+  const smallMovieCard = shallow(<SmallMovieCard
+    movie={film}
+  />);
+
+  const movieCard = smallMovieCard.find(`.small-movie-card`);
+  movieCard.simulate(`mouseEnter`);
+
+  setTimeout(() => {
+    expect(smallMovieCard.state(`isVideoPlaying`)).toEqual(true);
+  }, 1000);
+});
+
+it(`onMouseLeave event is correctly`, () => {
+  const film = films[0];
+
+  const smallMovieCard = shallow(<SmallMovieCard
+    movie={film}
+  />);
+
+  const movieCard = smallMovieCard.find(`.small-movie-card`);
+  movieCard.simulate(`mouseLeave`);
+
+  expect(smallMovieCard.state(`isVideoPlaying`)).toEqual(false);
+});
